9cf28daf3f6941f1159a841b69e806946da06754
arslanblcn <arslan.bilecen@gmail.com>
2021-12-31 21:06:44 +0300

    Created All Tables

--- setup.php
+++ setup.php
@@ -29,8 +29,32 @@
             personel_firstname VARCHAR(25) NOT NULL),
             personel_lastname VARCHAR(25) NOT NULL),
             personel_phone VARCHAR(13) NOT NULL)";
-        if($conn -> query($sql) === TRUE){echo "[+] Users' table has been created";}
+        if($conn -> query($sql) === TRUE){echo "[+] Restorants' table has been created";}
+        else{echo "[-] Error when creating table " . $conn->error;}
+    }
+    function createMenu($conn){
+        $sql = "CREATE TABLE restorant_menu (id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            food VARCHAR(45) NOT NULL,
+            food_img BLOB NOT NULL,
+            food_cost VARCHAR(5) NOT NULL,
+            food_number INT(30) NOT NULL,
+            food_material VARCHAR(20) NOT NULL,
+            restorant_id INT(4) NOT NULL)";
+        if($conn -> query($sql) === TRUE){echo "[+] Menu table has been created";}
+        else{echo "[-] Error when creating table " . $conn->error;}
+    }
+    function orderDetail($conn){
+        $sql = "CREATE TABLE restorant_menu (id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            order_date DATETIME NOT NULL,
+            total_cost INT(5) NOT NULL,
+            food_cost VARCHAR(5) NOT NULL,
+            user_id INT(5) NOT NULL,
+            restorant_menuid INT(4) NOT NULL)";
+        if($conn -> query($sql) === TRUE){echo "[+] Menu table has been created";}
         else{echo "[-] Error when creating table " . $conn->error;}
     }
 createUser($conn);
+createRestorant($conn);
+createMenu($conn);
+orderDetail($conn);
 ?>
