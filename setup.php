2677fdecd55a10a153c670b40560074e9efce7e9
arslanblcn <arslan.bilecen@gmail.com>
2021-12-31 20:54:56 +0300

    Restorant Table Created

--- setup.php
+++ setup.php
@@ -12,5 +12,25 @@
         if($conn -> query($sql) === TRUE){echo "[+] Users' table has been created";}
         else{echo "[-] Error when creating table " . $conn->error;}
     }
+
+    function createRestorant($conn){
+        $sql = "CREATE TABLE restorants (id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            restorant_name VARCHAR(60) NOT NULL,
+            phone_number VARCHAR(15) NOT NULL,
+            restorant_type VARCHAR(15) NOT NULL,
+            cit VARCHAR(20) NOT NULL,
+            city VARCHAR(20) NOT NULL,
+            ditrict VARCHAR(25) NOT NULL),
+            neighborhood VARCHAR(25) NOT NULL),
+            address VARCHAR(25) NOT NULL),
+            owner_firstname VARCHAR(25) NOT NULL),
+            owner_lastname VARCHAR(25) NOT NULL),
+            owner_phone VARCHAR(13) NOT NULL),
+            personel_firstname VARCHAR(25) NOT NULL),
+            personel_lastname VARCHAR(25) NOT NULL),
+            personel_phone VARCHAR(13) NOT NULL)";
+        if($conn -> query($sql) === TRUE){echo "[+] Users' table has been created";}
+        else{echo "[-] Error when creating table " . $conn->error;}
+    }
 createUser($conn);
 ?>
